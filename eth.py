6f9b65826e1db0a8b114d9451704d5313491932f
ascii bene <ascii_bene@protonmail.ch>
2025-04-09 02:11:52 -0400

    Now thats better

--- eth.py
+++ eth.py
@@ -4,6 +4,7 @@ import base58
 import requests
 import logging
 import os
+import json
 import time
 from eth_account import Account 
 import re
@@ -23,50 +24,66 @@ init(autoreset=True)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Constants
-RATE_LIMIT = 5# Max 1 request per second
-TIME_PERIOD = 1  # Time period in seconds for rate limit
+
+os.environ['ECC_BACKEND_CLASS'] = 'eth_keys.backends.NativeECCBackend'
+
+RATE_LIMIT = 3# Max 1 request per second
+TIME_PERIOD = 10  # Time period in seconds for rate limit
 WEI_PER_ETH = 1e10
+CHECK_TOKENS = False #TODO
 ES_KEY= "RJWQE7MKV6FCSGP41N8YHNMKJ9VATFN8ZD"
+ES_CHAINIDS={"etherscan": 1, 'bscscan': 56, 'ftmscan': 250, 'optimistic': 10, 'polygon': 137, 'arbitrum': 42161, 'gnosis': 100,'base': 8453,'avalanche': 43114,'zkSync': 324}
+   
 
-
+TOKEN_CONTRACTS = {"name", 'hexaddr'}
 etherscan_tags = ['result']
 
 # Adjust the check_balance function
 @sleep_and_retry
 @limits(calls=RATE_LIMIT, period=TIME_PERIOD)
-def check_api_eth(address): 
+def check_address_balance_eth(address,privkey): 
     try:
-        #Todo : send multiple address in 1 query.. use  |
-        url= f"https://api.etherscan.io/api?module=account&action=balance&address={address}&tag=latest&apikey={ES_KEY}"
-        start_time = time.time()
-        htmltext = urlopen(url, timeout=10).read().decode('utf-8')
-        end_time = time.time()
-        ping = end_time - start_time
-        if htmltext is None: raise("Empty reply from url")
-        
-        ethscan_info = {t: float(re.search(rf'{t}":"(\d+)', htmltext).group(1)) for t in etherscan_tags}
-        
-        ethscan_info['result'] /= WEI_PER_ETH
-        ethscan_info['ping'] = ping
-        return ethscan_info
+        wallets_multichain=[]
+        for chain,chainid in tqdm(ES_CHAINIDS.items(), colour='blue'):
+            #Todo : send multiple address in 1 query.. use  |
+            url= f"https://api.etherscan.io/v2/api?chainid={chainid}&module=account&action=balance&address={address}&tag=latest&apikey={ES_KEY}"
+            htmltext = urlopen(url, timeout=10).read().decode('utf-8')
+            if htmltext is None: raise("Empty reply from url")
+            
+            dat=json.loads(htmltext)
+            #ethscan_info = {t: float(re.search(rf'{t}":"(\d+)', htmltext).group(1)) for t in etherscan_tags}print(dat)
+           # dat['result'] = float(dat['result']) /  WEI_PER_ETH
+            del dat['status'] 
+            #dat['result'] = float(dat['result'])
+            dat['message'] = Fore.GREEN + dat['message'] + Style.RESET_ALL if dat['message'] == 'OK' else exit() 
+
+            dat['chain']=f"{chain} ({chainid})" 
+            dat['address'] = address
+            dat['private_key'] = privkey
+            wallets_multichain.append(dat)
+            time.sleep(0.25)
+        return wallets_multichain
     except Exception as e:
         print(f"Request failed for address {address}: {e}")
         return None
 
+
+
 def gen_wallet_eth():
     wallet = Account.create()
-    return wallet.key, wallet.address
+    #TODO fix encoding here for privaye key
+    return wallet.key.hex(), wallet.address
 
 def process_iteration():
     private_key, address = gen_wallet_eth()
     if not private_key or not address:
         raise(Error)
     
-    wallet_info = check_api_eth(address)
-    wallet_info['address']=address
-    wallet_info['priv_key']=private_key
-    
-    return wallet_info
+    wallets_info = check_address_balance_eth(address,private_key)
+    #wallet_info['address']=address
+    #wallet_info['priv_key']=private_key'
+
+    return wallets_info
 
 
 def load_passphrases(input_file):
@@ -93,30 +110,30 @@ def save_results(output_file,wallet_info):
 def main():
     input_file = os.getenv('INPUT_FILE', 'passphrases.txt')
     output_file = os.getenv('OUTPUT_FILE', 'wallets_with_balance.txt')
-    num_workers = int(os.getenv('NUM_WORKERS', 2))
+    num_workers = int(os.getenv('NUM_WORKERS', 1))
     #passphrases = load_passphrases(input_file)
     addrs_with_bal = []
     with ThreadPoolExecutor(max_workers=num_workers) as executor:
         MAX_ITER = int(argv[1]) or 250
         future_to_iter = {executor.submit(process_iteration): i for i in range(1,MAX_ITER)}
 
-        for future in tqdm(as_completed(future_to_iter), total=MAX_ITER, desc="Processing iterations"): 
+        for future in as_completed(future_to_iter): 
             try:
-                wallet_info = future.result()
-                
-                if wallet_info['result'] > 0:
-                    status = Fore.GREEN + "ACTIVE!" + Style.RESET_ALL
-                    save_results(output_file,wallet_info)
-                else:
-                    status = Fore.RED + "DEAD" + Style.RESET_ALL
-                
-                # Print the details with status
-                print('Ethereum Wallet data >>> -------------')
-                for key, val in wallet_info.items():
-                    print(f"{key}: {val}")
-                print(f"Status: {status}")
-              
-                print()
+                wallets = future.result()
+                for w in wallets:
+                    if float(w['result']) > 0:
+                        status = Fore.GREEN + "ACTIVE!" + Style.RESET_ALL
+                        save_results(output_file,w)
+                    else:
+                        status = Fore.RED + "DEAD" + Style.RESET_ALL
+                    
+                    # Print the details with status
+                    print(Fore.BLUE + f'Wallet data >>> {w['chain']} -------------'+ Style.RESET_ALL)
+                    for key, val in w.items():
+                        print(f"{key}: {val}")
+                    print(f"Status: {status}")
+                    print()
+
             except Exception as e:
                 print(f"Main Program Loop failed :  {e}")
 
