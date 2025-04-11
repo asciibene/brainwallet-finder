76845e56f2f9f10456eae01d7accbb2df178c594
asciibene <ascii_bene@protonmail.ch>
2025-04-11 15:13:12 -0400

    changes

--- eth.py
+++ eth.py
@@ -2,7 +2,6 @@ import hashlib
 import ecdsa
 import base58
 import requests
-import logging
 import os
 import json
 import time
@@ -20,24 +19,21 @@ from sys import argv
 # Initialize colorama
 init(autoreset=True)
 
-# Setup logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Constants
 
 os.environ['ECC_BACKEND_CLASS'] = 'eth_keys.backends.NativeECCBackend'
 
-RATE_LIMIT = 1# Max 1 request per second
+RATE_LIMIT = 3# Max 1 request per second
 TIME_PERIOD = 1  # Time period in seconds for rate limit
 WEI_PER_ETH = 1e10
 
 ES_KEY= "RJWQE7MKV6FCSGP41N8YHNMKJ9VATFN8ZD"
-ES_CHAINIDS={"etherscan": 1, 'bscscan': 56, 'ftmscan': 250, 'optimistic': 10, 'polygon': 137, 'arbitrum': 42161, 'gnosis': 100,'base': 8453,'avalanche': 43114,'zkSync': 324}
+ES_CHAINIDS={"etherscan":1 , 'ftmscan': 250, 'optimistic': 10, 'polygon': 137, 'arbitrum': 42161, 'gnosis': 100,'base': 8453,'avalanche': 43114,'zkSync': 324}
    
 CHECK_TOKENS = False #TODO
 TOKEN_CONTRACTS = {"name", 'hexaddr'}
-etherscan_tags = ['result']
-we_got_problems=False
+
 
 # Adjust the check_balance function
 @sleep_and_retry
@@ -48,7 +44,7 @@ def check_address_balance_eth(address,privkey):
         for chain,chainid in tqdm(ES_CHAINIDS.items(), colour='blue'):
             #Todo : send multiple address in 1 query.. use  |
             url= f"https://api.etherscan.io/v2/api?chainid={chainid}&module=account&action=balance&address={address}&tag=latest&apikey={ES_KEY}"
-            htmltext = urlopen(url, timeout=10).read().decode('utf-8')
+            htmltext = urlopen(url, timeout=6).read().decode('utf-8')
             if htmltext is None: raise("Empty reply from url")
             
             dat=json.loads(htmltext)
@@ -62,7 +58,7 @@ def check_address_balance_eth(address,privkey):
             dat['address'] = address
             dat['private_key'] = privkey
             wallets_multichain.append(dat)
-            time.sleep(0.10)
+            time.sleep(0.05)
         return wallets_multichain
     except Exception as e:
         print(f"Request failed for address {address}: {e}")
@@ -72,7 +68,6 @@ def check_address_balance_eth(address,privkey):
 
 def gen_wallet_eth():
     wallet = Account.create()
-    #TODO fix encoding here for private key
     return wallet.key.hex(), wallet.address
 
 def process_iteration():
@@ -85,17 +80,6 @@ def process_iteration():
     #wallet_info['priv_key']=private_key'
 
     return wallets_info
-
-
-def load_passphrases(input_file):
-    try:
-        print("Loading passphrases from file..")
-        with open(input_file, 'r') as f:
-            passphrases = [line.strip() for line in tqdm(f)]
-            return passphrases
-    except FileNotFoundError:
-        print(f"Input file {input_file} not found.")
-        return None
 
 def save_results(output_file,wallet_info):
     try:
@@ -110,15 +94,16 @@ def save_results(output_file,wallet_info):
 
 
 def main():
-    input_file = os.getenv('INPUT_FILE', 'passphrases.txt')
     output_file = os.getenv('OUTPUT_FILE', 'wallets_with_balance.txt')
     num_workers = int(os.getenv('NUM_WORKERS', 1))
     #passphrases = load_passphrases(input_file)
     addrs_with_bal = []
+    we_got_problems=False
     with ThreadPoolExecutor(max_workers=num_workers) as executor:
         MAX_ITER = int(argv[1]) or 250
-        future_to_iter = {executor.submit(process_iteration): i for i in range(1,MAX_ITER)}
 
+        future_to_iter = {executor.submit(process_iteration): i for i in range(1,MAX_ITER)}
+        found_wallets = 0
         for future in as_completed(future_to_iter): 
             try:
                 wallets = future.result()
@@ -126,18 +111,24 @@ def main():
                     if float(w['result']) > 0:
                         status = Fore.GREEN + "ACTIVE!" + Style.RESET_ALL
                         save_results(output_file,w)
+                        found_wallets += 1
                     else:
                         status = Fore.RED + "DEAD" + Style.RESET_ALL
                     
                     # Print the details with status
                     print(Fore.BLUE + f'Wallet data >>> {w["chain"]}' + Style.RESET_ALL )
                     for key, val in w.items():
+                        if key == "result" and int(val) > 0:
+                            key = Fore.CYAN + key + Style.RESET_ALL
+                            val = Back.YELLOW + Fore.MAGENTA + val + Style.RESET_ALL
+
                         print(f"{key}: {val}")
                     print(f"Status: {status}")
-                    print()
+                print((Fore.RED + "We have got an error.." + Style.RESET_ALL ) if we_got_problems else "")
 
             except Exception as e:
                 print(f"Main Program Loop failed :  {e}")
+                we_got_problems = True
 
     print(f"Processing complete. Results saved to {output_file}")
 
